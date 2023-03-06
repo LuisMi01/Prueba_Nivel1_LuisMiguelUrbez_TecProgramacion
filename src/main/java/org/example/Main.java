151be6907704cf8f4026561648d9fcb117af4d81
LuisMi01 <lmurbezv@gmail.com>
2023-03-06 18:19:02 +0100

    tsi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,7 @@
 package org.example;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -14,11 +17,11 @@ public class Main {
             tabla.add(new LinkedList<>());
         }
     }
-
     public void agregar(String dato) {
         int posicion = funcion_hash(dato, tamano);
         LinkedList<String> lista = tabla.get(posicion);
         if (lista.contains(dato)) {
+            System.out.println("Se agregó el dato " + dato + " en la posicion " + posicion);
             return; // El elemento ya se encuentra en la tabla
         }
         lista.add(dato);
@@ -29,6 +32,7 @@ public class Main {
         LinkedList<String> lista = tabla.get(posicion);
         if (lista.contains(dato)) {
             lista.remove(dato);
+            System.out.println("El dato se elimino correctamente");
             return dato;
         }
         return null; // El elemento no se encontró en la tabla
@@ -38,6 +42,7 @@ public class Main {
         int posicion = funcion_hash(dato, tamano);
         LinkedList<String> lista = tabla.get(posicion);
         if (lista.contains(dato)) {
+            System.out.println("Dato encontrado en la posicion " + posicion);
             return posicion;
         }
         return -1; // El elemento no se encontró en la tabla
@@ -59,6 +64,26 @@ public class Main {
         for (LinkedList<String> lista : tabla) {
             contador += lista.size();
         }
+        System.out.println("Hay un total de " + contador + " elementos en la tabla");
         return contador;
+    }
+     public void agregar_desde_csv(String archivo) {
+        String csvSeparator = ",";
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] datos = line.split(csvSeparator);
+                for (String dato : datos) {
+                    tabla.agregar(dato);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        tabla tabla_csv = new tabla(10);
+        tabla_csv.agregar_desde_csv("/Users/luismiguelurbez/Downloads/palabras.csv");
     }
 }
