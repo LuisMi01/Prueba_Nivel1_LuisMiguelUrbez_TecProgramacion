be19bc0385c83c79667efae6da494e7d589e84fa
LuisMi01 <lmurbezv@gmail.com>
2023-03-06 18:28:11 +0100

    funciona

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Main {
-    // Representación de la tabla como un arreglo de listas enlazadas
+
     private ArrayList<LinkedList<String>> tabla;
     private int tamano;
 
@@ -22,11 +22,10 @@ public class Main {
         LinkedList<String> lista = tabla.get(posicion);
         if (lista.contains(dato)) {
             System.out.println("Se agregó el dato " + dato + " en la posicion " + posicion);
-            return; // El elemento ya se encuentra en la tabla
+            return;
         }
         lista.add(dato);
     }
-
     public String quitar(String dato) {
         int posicion = funcion_hash(dato, tamano);
         LinkedList<String> lista = tabla.get(posicion);
@@ -35,27 +34,24 @@ public class Main {
             System.out.println("El dato se elimino correctamente");
             return dato;
         }
-        return null; // El elemento no se encontró en la tabla
+        return null;
     }
-
     public int buscar(String dato) {
         int posicion = funcion_hash(dato, tamano);
         LinkedList<String> lista = tabla.get(posicion);
         if (lista.contains(dato)) {
-            System.out.println("Dato encontrado en la posicion " + posicion);
+            System.out.println("Dato" + dato +"encontrado en la posicion " + posicion);
             return posicion;
         }
-        return -1; // El elemento no se encontró en la tabla
+        return -1;
     }
-
     private int funcion_hash(String dato, int tamano_tabla) {
-        // Implementación de la función hash utilizando el primer carácter del dato
+
         int ascii = (int) dato.charAt(0);
         return ascii % tamano_tabla;
     }
-
     private int sondeo(int posicion, int tamano_tabla) {
-        // Implementación de la función de sondeo utilizando la técnica lineal
+        System.out.println("Se aplicó el método de sondeo");
         return (posicion + 1) % tamano_tabla;
     }
 
@@ -67,23 +63,26 @@ public class Main {
         System.out.println("Hay un total de " + contador + " elementos en la tabla");
         return contador;
     }
-     public void agregar_desde_csv(String archivo) {
-        String csvSeparator = ",";
-        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] datos = line.split(csvSeparator);
-                for (String dato : datos) {
-                    tabla.agregar(dato);
-                }
+    public void leer_archivo_csv(String nombre_archivo) {
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(nombre_archivo));
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                agregar(linea);
             }
+            lector.close();
+            System.out.println("Se leyó el archivo correctamente");
         } catch (IOException e) {
+            System.out.println("Error al leer el archivo");
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) {
-        tabla tabla_csv = new tabla(10);
-        tabla_csv.agregar_desde_csv("/Users/luismiguelurbez/Downloads/palabras.csv");
+        int tamano_tabla = 28;
+        Main tabla = new Main(tamano_tabla);
+        tabla.leer_archivo_csv("/Users/luismiguelurbez/Downloads/palabras.csv");
+        tabla.cantidad_elementos();
+        tabla.sondeo(5, 28);
+        tabla.buscar("Hola");
     }
 }
